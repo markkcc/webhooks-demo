e166e14ee14e9eaf2067094b76d68ca5508128ba
mugl <mu.gl>
2020-05-19 23:06:35 -0400

    catch no events exception when relation does not exist

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,10 @@ class DashboardController < ApplicationController
   before_action :authenticate_user_action!
 
   def index
-    @no_events = Event.count == 0
+    begin
+      @no_events = Event.count == 0
+    rescue ActiveRecord::StatementInvalid => si
+      @no_events = true
+    end
   end
 end
